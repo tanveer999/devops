bb0f2ae670f6d9ab7cb31645f73b68870a552db8
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2024-12-07 13:44:40 +0530

    update: add packages

--- archlinux/initial_setup.sh
+++ archlinux/initial_setup.sh
@@ -2,10 +2,19 @@
 
 set -x
 
-echo "Installing required packages"
-pacman -Sy
-pacman -Sy flatpak fastfetch firefox less git code docker minikube ntfs-3g tmux stow fzf
+echo "Installing required packages........"
 
-echo "Configuring git"
+sudo pacman -Sy
+sudo pacman -Sy flatpak fastfetch firefox less git code docker minikube ntfs-3g tmux stow fzf
+
+echo "Installing flatpaks............."
+flatpak install flathub org.kde.okular
+flatpak install flathub md.obsidian.Obsidian
+flatpak install flathub com.brave.Browser
+
+echo "Configuring git........"
 git config --global user.name "Tanveer Ahmed"
 git config --global user.email "tanveerahmed2619@gmail.com"
+
+echo "Starting services......"
+sudo systemctl enable bluetooth
