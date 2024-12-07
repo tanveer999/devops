044a70c259f9ed9246a3d7e857ac3ad1b91eb13c
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2024-12-07 13:48:57 +0530

    udpate: flatpak options

--- archlinux/initial_setup.sh
+++ archlinux/initial_setup.sh
@@ -8,9 +8,9 @@ sudo pacman -Sy
 sudo pacman -Sy flatpak fastfetch firefox less git code docker minikube ntfs-3g tmux stow fzf
 
 echo "Installing flatpaks............."
-flatpak install flathub org.kde.okular
-flatpak install flathub md.obsidian.Obsidian
-flatpak install flathub com.brave.Browser
+flatpak install --noninteractive -y flathub org.kde.okular
+flatpak install --noninteractive -y flathub md.obsidian.Obsidian
+flatpak install --noninteractive -y flathub com.brave.Browser
 
 echo "Configuring git........"
 git config --global user.name "Tanveer Ahmed"
