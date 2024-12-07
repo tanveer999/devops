c847a72436c90e1f8f5f92a7fa1772fc7d07d2cb
Tanveer Ahmed <tanveer@test.com>
2024-12-07 15:34:57 +0530

    add: font install step

--- archlinux/initial_setup.sh
+++ archlinux/initial_setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
 set -x
+
+FONT_DIR="~/.local/share/fonts"
+
+mkdir -p ~/tmp
 
 echo "Installing required packages........"
 
 sudo pacman -Sy
-sudo pacman -Sy --noconfirm flatpak fastfetch firefox less git code docker minikube ntfs-3g tmux stow fzf tree
+sudo pacman -Sy --noconfirm flatpak fastfetch firefox less git code docker minikube ntfs-3g tmux stow fzf tree unzip
 
 echo "Installing flatpaks............."
 flatpak install --noninteractive -y flathub org.kde.okular
@@ -18,3 +21,9 @@ git config --global user.email "tanveer@test.com"
 
 echo "Starting services......"
 sudo systemctl enable bluetooth
+
+echo "starship setup ........."
+mkdir -p $FONT_DIR
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CodeNewRoman.zip -o ~/tmp/codenewroman.zip
+unzip ~/tmp/codenewroman.zip -d $FONT_DIR
+curl -sS https://starship.rs/install.sh | sh
