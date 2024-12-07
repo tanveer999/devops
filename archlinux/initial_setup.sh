840e8a449d3e9bda38ff8e362d20fdbda651dc93
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2024-12-07 13:36:26 +0530

    update: archlinux setup

--- archlinux/initial_setup.sh
+++ archlinux/initial_setup.sh
@@ -4,6 +4,8 @@ set -x
 
 echo "Installing required packages"
 pacman -Sy
-pacman -Sy flatpak fastfetch firefox less git code docker minikube ntfs-3g tmux
+pacman -Sy flatpak fastfetch firefox less git code docker minikube ntfs-3g tmux stow fzf
 
+echo "Configuring git"
 git config --global user.name "Tanveer Ahmed"
+git config --global user.email "tanveerahmed2619@gmail.com"
