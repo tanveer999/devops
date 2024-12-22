b762c19dfaee97ffac4d5faaa71786ad7b660d6f
Tanveer Ahmed <tanveer@test.com>
2024-12-22 18:54:16 +0530

    arch nvim installed fd for neotree

--- archlinux/initial_setup.sh
+++ archlinux/initial_setup.sh
@@ -9,7 +9,7 @@ mkdir -p $TMP_DIR
 echo "Installing required packages........"
 
 sudo pacman -Sy
-sudo pacman -Sy --noconfirm flatpak fastfetch firefox less git docker minikube kubectl ntfs-3g tmux stow tree unzip okular obsidian xclip zsh qbittorrent neovim fzf flameshot vagrant virtualbox gwenview  alacritty
+sudo pacman -Sy --noconfirm flatpak fastfetch firefox less git docker minikube kubectl ntfs-3g tmux stow tree unzip okular obsidian xclip zsh qbittorrent neovim fzf flameshot vagrant virtualbox gwenview  alacritty fd
 
 echo "Configure yay ......"
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
