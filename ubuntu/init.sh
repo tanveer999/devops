e7dd4c7debb856fa019eac363212c5885524b46c
Tanveer Ahmed <tanveer@test.com>
2025-01-05 18:48:25 +0530

    ubuntu init

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -4,7 +4,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y build-essential
 
-sudo apt install -y tmux xclip curl alacritty neovim ntfs-3g git stow zsh gnome-shell-extension-manager nvtop transmission
+sudo apt install -y tmux xclip curl alacritty neovim ntfs-3g git stow zsh gnome-shell-extension-manager nvtop transmission python3-venv python3-pip ripgrep fd-find
 
 # tmux plugin
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
