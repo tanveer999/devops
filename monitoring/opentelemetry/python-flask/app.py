b4b9d27e7d5c07b69146aba34a1894ca6075a038
Tanveer Ahmed <tanveer@test.com>
2025-04-15 22:16:34 +0530

    added manual instrumentation

--- monitoring/opentelemetry/python-flask/app.py
+++ monitoring/opentelemetry/python-flask/app.py
@@ -2,6 +2,11 @@ from random import randint
 from flask import Flask, request, jsonify
 import logging
 
+from opentelemetry import trace
+
+# Acquire a tracer
+tracer = trace.get_tracer("diceroller.tracer")
+
 # Initialize Flask app
 app = Flask(__name__)
 
@@ -30,7 +35,12 @@ def roll() -> int:
     """
     Simulates rolling a six-sided dice and returns the result.
     """
-    return randint(1, 6)
+
+    # This creates a new span that's the child of the current one
+    with tracer.start_as_current_span("roll") as rollspan:
+        res = randint(1, 6)
+        rollspan.set_attribute("roll.value", res)
+        return res
 
 
 if __name__ == "__main__":
